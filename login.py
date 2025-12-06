5f22ce9d97b4dc1c1a0a3d3d197822850b842188
Eric Taylor <erictaylor4954@gmail.com>
2025-12-06 12:33:26 -0800

    fixed:auth got:playlists helped:humanity saved:pokemons defeated:googauth

--- login.py
+++ login.py
@@ -8,6 +8,7 @@ from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
 from google.cloud import secretmanager
+from google.oauth2.credentials import Credentials
 
 load_dotenv()
 
@@ -41,8 +42,8 @@ def get_client_secrets():
 def index():
     if 'credentials' not in session:
         return '<h1>YouTube OAuth</h1><a href="/login">Login with Google</a>'
-    return '<h1>Logged in!</h1><a href="/channel">View Channel</a> | <a href="/logout">Logout</a>'
-    """
+    # return '<h1>Logged in!</h1><a href="/channel">View Channel</a> | <a href="/logout">Logout</a>'
+    
     return '''
     <h1>Logged in!</h1>
     <ul>
@@ -51,42 +52,42 @@ def index():
         <li><a href="/logout">Logout</a></li>
     </ul>
     '''
-    """
 
-    @app.route('/login')
-    def login():
-        client_config = get_client_secrets()
-        
-        flow = Flow.from_client_config(
-            client_config,
-            scopes=SCOPES,
-            redirect_uri=url_for('callback', _external=True)
-        )
-        auth_url, state = flow.authorization_url(access_type='offline')
-        session['state'] = state
-        return redirect(auth_url)
+@app.route('/login')
+def login():
+    client_config = get_client_secrets()
+    
+    flow = Flow.from_client_config(
+        client_config,
+        scopes=SCOPES,
+        redirect_uri=url_for('callback', _external=True)
+    )
+
+    auth_url, state = flow.authorization_url(access_type='offline')
+    session['state'] = state
+    return redirect(auth_url)
 
 
-    @app.route('/callback')
-    def callback():
-        client_config = get_client_secrets()
-        
-        flow = Flow.from_client_config(
-            client_config,
-            scopes=SCOPES,
-            redirect_uri=url_for('callback', _external=True)
-        )
-        
-        flow.fetch_token(authorization_response=request.url)
-        
-        session['credentials'] = {
-            'token': flow.credentials.token,
-            'refresh_token': flow.credentials.refresh_token,
-            'token_uri': flow.credentials.token_uri,
-            'client_id': flow.credentials.client_id,
-            'client_secret': flow.credentials.client_secret,
-            'scopes': flow.credentials.scopes
-        }
+@app.route('/callback')
+def callback():
+    client_config = get_client_secrets()
+    
+    flow = Flow.from_client_config(
+        client_config,
+        scopes=SCOPES,
+        redirect_uri=url_for('callback', _external=True)
+    )
+    
+    flow.fetch_token(authorization_response=request.url)
+    
+    session['credentials'] = {
+        'token': flow.credentials.token,
+        'refresh_token': flow.credentials.refresh_token,
+        'token_uri': flow.credentials.token_uri,
+        'client_id': flow.credentials.client_id,
+        'client_secret': flow.credentials.client_secret,
+        'scopes': flow.credentials.scopes
+    }
 
     return redirect('/')
 
@@ -94,27 +95,25 @@ def index():
 def channel():
     if 'credentials' not in session:
         return redirect('/login')
-    
-    from google.oauth2.credentials import Credentials
+
     creds = Credentials(**session['credentials'])
     youtube = build('youtube', 'v3', credentials=creds)
-    
+
     response = youtube.channels().list(part='snippet,statistics', mine=True).execute()
     channel = response['items'][0]
-    
+
     return f'''
     <h1>{channel['snippet']['title']}</h1>
     <p>Subscribers: {channel['statistics'].get('subscriberCount', 'Hidden')}</p>
     <p>Views: {channel['statistics']['viewCount']}</p>
     <p><a href="/">Home</a></p>
     '''
-"""
+
 @app.route('/playlists')
 def playlists():
     if 'credentials' not in session:
         return redirect('/login')
     
-    from google.oauth2.credentials import Credentials
     creds = Credentials(**session['credentials'])
     youtube = build('youtube', 'v3', credentials=creds)
     
@@ -140,7 +139,6 @@ def playlists():
     
     html += '<p><a href="/channel">Back to Channel</a> | <a href="/">Home</a></p>'
     return html
-"""
 
 @app.route('/logout')
 def logout():
@@ -148,5 +146,12 @@ def logout():
     return redirect('/')
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == '__main__': 
+    print('Starting Flask app...')
+    print('Visit http://localhost:5000 in your browser')
+    print('Available routes:')
+    
+    for rule in app.url_map.iter_rules():
+        print(f"  {rule.endpoint}: {rule.rule}")
+    
+    app.run(debug=True, host='localhost', port=5000)
